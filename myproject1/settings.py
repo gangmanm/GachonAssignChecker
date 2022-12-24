0e450d9fc92b14d9bd798fbb9aac0ed1feadaa5b
gangmanm <97601109+gangmanm@users.noreply.github.com>
2022-12-25 03:24:15 +0900

    e

--- myproject1/settings.py
+++ myproject1/settings.py
@@ -133,4 +133,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
